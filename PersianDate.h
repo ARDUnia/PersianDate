31222e64e875553f6546c027283a1bec5b37f2d6
Hamidreza Milaninia <milaninia.h@gmail.com>
2026-05-27 15:01:09 +0330

    Update PersianDate.h
    PersianDate v2.1.0

--- PersianDate.h
+++ PersianDate.h
@@ -1,6 +1,6 @@
 /*
   PersianDate.h - Library for Gregorian to Persian (Jalali) date conversion and vice versa.
-  Version 2.1.0
+  Version 2.1.1
   Author: Hamidreza Milaninia (ARDUnia Agency)
   This library is independent of RTClib and can convert both ways.
 */
